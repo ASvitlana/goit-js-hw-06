97c52d4417386ae1054bee475a41625b6cb4f6e0
Светлана Петровна Андрела <104157616+ASvitlana@users.noreply.github.com>
2022-08-30 12:37:22 +0300

    ДЗ - 6, 1-10

--- js/task-10.js
+++ js/task-10.js
@@ -1,73 +1,32 @@
-function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
-}
 
-const boxes = document.querySelector('#boxes');
+const inputNum = document.querySelector('#controls>input');
 const btnCreate = document.querySelector('button[data-create]');
 const btnDestroy = document.querySelector('button[data-destroy]');
-const inputNum = document.querySelector('#controls input');
+const boxes = document.querySelector('#boxes');
 
-// let number;
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+};
 
-// inputNum.addEventListener('input', (event) => {
-//   number = event.currentTarget.value;
-//   return number; 
-// });
-
-// function createBoxes(number) {
-//   const elementsToAdd = [];
-//   // let number;
-
-//   // inputNum.addEventListener('input', (event) => {
-//   //   number = event.currentTarget.value;
-//   //   return number; 
-//   // });
-
-//   for (let i = 1; i <= number; i += 1) {
-//     const box = document.createElement('div');
-//     box.style.width = `${30 + 10 * (i - 1)}px`;
-//     box.style.height = `${30 + 10 * (i - 1)}px`;
-//     box.style.backgroundColor = `${getRandomHexColor()}`;
-//     elementsToAdd.push(box);
-//   }
-
-//   boxes.append(...elementsToAdd);
-// };
-
-// const destroyBoxes = () => {
-//   boxes.innerHTML = '';
-// };
-
-// btnCreate.addEventListener('click', createBoxes);
-// btnDestroy.addEventListener('click', destroyBoxes);
-
-
-console.log(inputNum);
-
-inputNum.addEventListener('input', (event) => {
-  // console.log(event.currentTarget.value);
-  // return event.currentTarget.value;
-
-  let num = event.currentTarget.value;
-  console.log(num);
-});
-// let num = 5;
-
-function createBoxes(num) {
-    // const elementsToAdd = [];
-    
-    for (let i = 1; i <= num; i += 1) {
-      const markupEl = `<div style = 'background-color: ${getRandomHexColor()}; width = ${30 + 10 * (i - 1)}px; height = ${30 + 10 * (i - 1)}px'></div>`;
-      boxes.innerHTML += markupEl;
-      // elementsToAdd.push(markupEl);
-    }
-  
-    // boxes.innerHTML = elementsToAdd.join('');
+const createBoxes = amount => {
+  const elementsToAdd = [];
+  for (let i = 0; i < amount; i += 1) {
+    const box = document.createElement('div');
+    box.style.height = `${30 + 10 * i}px`;
+    box.style.width = `${30 + 10 * i}px`;
+    box.style.background = getRandomHexColor();
+    elementsToAdd.push(box);
   };
+  return elementsToAdd;
+};
 
 const destroyBoxes = () => {
   boxes.innerHTML = '';
 };
 
-btnCreate.addEventListener('click', createBoxes());
+btnCreate.addEventListener('click', () => {
+  let boxesToAdd = createBoxes(inputNum.value);
+  boxes.append(...boxesToAdd);
+});
+
 btnDestroy.addEventListener('click', destroyBoxes);
